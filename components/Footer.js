bee7faa57e37e182ac90e3ff1f991e4c3e750cd6
KevzPeter <ultimatekevin1@gmail.com>
2021-11-21 19:07:57 +0530

    added Elapsed Time + Footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a href="https://github.com/kevzpeter" target="_blank" rel="no-referrer">
+        @kevzpeter
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
--- components/Solve.js
+++ components/Solve.js
@@ -1,39 +1,51 @@
 import styles from "../styles/Game.module.scss";
 import solveSudoku from "../lib/solveSudoku";
 import initSudoku from "../lib/initSudoku";
+import Timer from "./Timer";
+import { useState } from "react";
 
 const Solve = (props) => {
+  const [start, setStart] = useState(null);
+  const [end, setEnd] = useState(null);
+
   const handleClick = (choice) => {
     if (choice === "solve") {
+      setStart(Math.floor(Date.now()));
       let solvedSquares = solveSudoku(props.squares);
       props.setSquares(solvedSquares);
       props.setSolved(true);
+      setEnd(Math.floor(Date.now()));
     } else {
+      setStart(null);
+      setEnd(null);
       props.setSquares(initSudoku);
       props.setSolved(false);
     }
   };
 
   return (
-    <div className={styles.solve}>
-      <button
-        id={styles.solvebtn}
-        disabled={props.solved}
-        onClick={() => {
-          handleClick("solve");
-        }}
-      >
-        Sudo Solve 🤠
-      </button>
-      <button
-        id={styles.resetbtn}
-        onClick={() => {
-          handleClick("reset");
-        }}
-      >
-        Reset Board 🔄
-      </button>
-    </div>
+    <>
+      <div className={styles.solve}>
+        <button
+          id={styles.solvebtn}
+          disabled={props.solved}
+          onClick={() => {
+            handleClick("solve");
+          }}
+        >
+          Sudo Solve 🤠
+        </button>
+        <button
+          id={styles.resetbtn}
+          onClick={() => {
+            handleClick("reset");
+          }}
+        >
+          Reset Board 🔄
+        </button>
+      </div>
+      <Timer {...{ start, end }} />
+    </>
   );
 };
 
